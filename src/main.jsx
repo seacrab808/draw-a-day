301263e22f57b4bb9bcb0bd96d80369edc39379b
seacrab808 <seacrab808@naver.com>
2025-09-11 16:18:11 +0900

    Feat: 초기 라우팅 세팅

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import "./reset.css";
+import "./style.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,9 @@
+import Home from "../../components/layout/Home";
+
+export default function HomePage() {
+  return (
+    <div>
+        <Home />
+    </div>
+  )
+}
