f1b6c66f9e06dac3fb374b5d21641ffbcc7ec04b
seacrab808 <seacrab808@naver.com>
2025-09-17 16:53:07 +0900

    fix conflict

--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
 import React from 'react'
 import Landing from '../../components/Landing/Landing'
-=======
-import Landing from "../../components/Landing/Landing";
->>>>>>> e2b756513479ae2a2d8db5430b3eb5e26137621a
 
 export default function LandingPage() {
   return (
     <div>
-<<<<<<< HEAD
       <Landing />
-=======
-        <Landing />
->>>>>>> e2b756513479ae2a2d8db5430b3eb5e26137621a
     </div>
   )
 }
